65685e4aaecb303068810a8cd6a110fdc702fc04
Ayush Giri <ayushgiri9988777@gmail.com>
2025-11-30 10:56:16 +0530

    Time: 64 ms (92.18%) | Memory: 86.7 MB (95.93%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-      int n = nums.size();
+     int n = nums.size();
     long long total = 0;
     for (int x : nums) total += x;
     int r = total % p;
     if (r == 0) return 0;
 
-    vector<int> pref(n+1, 0);
-    long long running = 0;
-    for (int i = 0; i < n; ++i) {
-        running += nums[i];
-        pref[i+1] = (int)(running % p);
-    }
-
-    unordered_map<int, vector<int>> pos;
-    pos.reserve(n*2);
-    for (int i = 0; i <= n; ++i) {
-        pos[pref[i]].push_back(i);
-    }
-
+    unordered_map<int,int> last; // prefix_mod -> latest index
+    last.reserve(n*2);
+    last[0] = 0;
     int ans = n + 1;
+    long long running = 0;
     for (int j = 1; j <= n; ++j) {
-        int need = (pref[j] - r) % p;
+        running += nums[j-1];
+        int curr = (int)(running % p);
+        int need = curr - r;
+        need %= p;
         if (need < 0) need += p;
-        auto it = pos.find(need);
-        if (it == pos.end()) continue;
-        // find largest index i < j
-        auto &vec = it->second; // already in increasing order
-        auto ub = upper_bound(vec.begin(), vec.end(), j-1);
-        if (ub != vec.begin()) {
-            --ub;
-            int i = *ub;
-            if (!(i == 0 && j == n)) { // don't remove whole array
+        auto it = last.find(need);
+        if (it != last.end()) {
+            int i = it->second;
+            if (!(i == 0 && j == n)) // don't remove whole array
                 ans = min(ans, j - i);
-            }
         }
+        // store/update latest index for this prefix mod
+        last[curr] = j;
     }
     return (ans <= n-1) ? ans : -1;
     }
