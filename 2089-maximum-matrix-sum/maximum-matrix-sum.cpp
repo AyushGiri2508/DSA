884b395887f7676c994423938fbd8e8b64b6821b
Ayush Giri <ayushgiri9988777@gmail.com>
2026-01-05 20:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 39.2 MB (25.94%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long totalSum = 0;
+        int negCount = 0;
+        int minAbs = INT_MAX;
+        for (auto &row : matrix) {
+            for (int val : row) {
+                if (val < 0) negCount++;
+                totalSum += abs(val);
+                minAbs = min(minAbs, abs(val));
+            }
+        }
+        if (negCount % 2 == 1) {
+            totalSum -= 2 * minAbs;
+        }
+
+        return totalSum;
+    }
+};
