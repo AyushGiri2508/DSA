52fe1170a579f5dc729424940c38387872c5a71a
Ayush Giri <ayushgiri9988777@gmail.com>
2026-01-14 18:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (66.92%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+
+int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+    
+ int slow = n, fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+
+    }
+};
