f4802bc67825198ebe530163ac9a502912ea07d6
Ayush Giri <ayushgiri9988777@gmail.com>
2026-03-21 08:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (37.82%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int solve(vector<int>& nums){
+    int start=0;
+    int end=nums.size()-1;
+    int mid=start+(end-start)/2;
+    while(start<end){
+        if(nums[mid]<nums[mid+1]){
+           
+             start=mid+1;
+        }
+        else{
+            end=mid;
+        }
+        mid=start+(end-start)/2;
+    }
+    return start;
+
+}
+    int findPeakElement(vector<int>& nums) {
+       int ans=solve(nums);
+       return ans;
+    }
+};
