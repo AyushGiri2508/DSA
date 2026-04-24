8f0899e4783bed33bb72b7ecdd3fe84f4ee2dd35
Ayush Giri <ayushgiri9988777@gmail.com>
2026-04-25 00:43:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (65.27%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int left=0;
+        int right=0;
+        int cnt=0;
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='L')left++;
+            else if(moves[i]=='R') right++;
+            else cnt++;
+        }
+        return abs(left-right)+cnt;
+    }
+};
