af9bb0fdb0453085ad76050d5c4a041732c4e5b3
Ayush Giri <ayushgiri9988777@gmail.com>
2026-05-05 12:43:29 +0530

    Time: 71 ms (60.56%) | Memory: 105.3 MB (96.43%) - LeetSync

--- 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -1,35 +1,68 @@
+// class Solution {
+// public:
+// bool ispossible(vector<int>& dist, double hour,int target){
+//     double time=0;
+    
+//     for(int i=0;i<dist.size();i++){
+//        double  cnt=(double)dist[i]/target;
+//        if (i != dist.size() - 1)
+//                 time += ceil(cnt);   // important
+//             else
+//                 time += cnt;
+//     }
+//       return time <= hour;
+   
+// }
+//     int minSpeedOnTime(vector<int>& dist, double hour) {
+//         int start=1;
+//         int end=1e7;
+//         int n=dist.size();
+     
+//         int ans=-1;
+//         while(start<=end){
+//             double mid= start+(end-start)/2;
+//             if(ispossible(dist,hour,mid)){
+//                 ans=mid;
+           
+//                 end=mid-1;
+//             }
+//             else{
+//                     start=mid+1; 
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-bool ispossible(vector<int>& dist, double hour,int target){
-    double time=0;
-    
-    for(int i=0;i<dist.size();i++){
-       double  cnt=(double)dist[i]/target;
-       if (i != dist.size() - 1)
-                time += ceil(cnt);   // important
+    bool ispossible(vector<int>& dist,int speed,double hour){
+        double ans=0;
+        for(int i=0;i<dist.size();i++){
+            double d=dist[i]*1.0/speed;
+            if(i!=dist.size()-1)
+            ans=ans+ceil(d);
             else
-                time += cnt;
-    }
-      return time <= hour;
-   
-}
-    int minSpeedOnTime(vector<int>& dist, double hour) {
-        int start=1;
-        int end=1e7;
-        int n=dist.size();
-     
-        int ans=-1;
-        while(start<=end){
-            double mid= start+(end-start)/2;
-            if(ispossible(dist,hour,mid)){
-                ans=mid;
-           
-                end=mid-1;
-            }
-            else{
-                    start=mid+1; 
-            }
+            ans+=d;
+            if(ans>hour)
+            return false;
         }
-        return ans;
+       if(ans<=hour)
+       return true;
+       return false;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int i=1;
+        int j=1e7;
+        int minspeed=-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(ispossible(dist,mid,hour)){
+               minspeed=mid;
+               j=mid-1;
+            }
+            else
+            i=mid+1;
+        }
+        return minspeed;
     }
 };
