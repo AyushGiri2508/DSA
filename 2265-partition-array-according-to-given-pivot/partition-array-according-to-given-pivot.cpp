61127e945ca0142b6352d79a5ffa1b31ec7138aa
Ayush Giri <ayushgiri9988777@gmail.com>
2026-06-08 10:11:07 +0530

    Time: 7 ms (62.22%) | Memory: 133.3 MB (62.93%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,31 +1,57 @@
+// class Solution {
+// public:
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         vector<int> ans;
+//         vector<int> ans1;
+//         vector<int> ans2;
+//         int cnt = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] < pivot) {
+//                 ans1.push_back(nums[i]);
+//             }
+//             if (nums[i] >= pivot) {
+//                 if (nums[i] == pivot) {
+//                     cnt++;
+//                 } else {
+//                     ans2.push_back(nums[i]);
+//                 }
+//             }
+//         }
+//             for (int i = 0; i < ans1.size(); i++) {
+//                 ans.push_back(ans1[i]);
+//             }
+//             while (cnt--) {
+//                 ans.push_back(pivot);
+//             }
+//             for (int i = 0; i < ans2.size(); i++) {
+//                 ans.push_back(ans2[i]);
+//             }
+//             return ans;
+//         }
+//     };
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> ans;
-        vector<int> ans1;
-        vector<int> ans2;
+        // ans.reserve(nums.size());
+
         int cnt = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < pivot) {
-                ans1.push_back(nums[i]);
-            }
-            if (nums[i] >= pivot) {
-                if (nums[i] == pivot) {
-                    cnt++;
-                } else {
-                    ans2.push_back(nums[i]);
-                }
-            }
+
+        for (int x : nums) {
+            if (x < pivot)
+                ans.push_back(x);
+            else if (x == pivot)
+                cnt++;
         }
-            for (int i = 0; i < ans1.size(); i++) {
-                ans.push_back(ans1[i]);
-            }
-            while (cnt--) {
-                ans.push_back(pivot);
-            }
-            for (int i = 0; i < ans2.size(); i++) {
-                ans.push_back(ans2[i]);
-            }
-            return ans;
+
+        while (cnt--)
+            ans.push_back(pivot);
+
+        for (int x : nums) {
+            if (x > pivot)
+                ans.push_back(x);
         }
-    };
+
+        return ans;
+    }
+};
