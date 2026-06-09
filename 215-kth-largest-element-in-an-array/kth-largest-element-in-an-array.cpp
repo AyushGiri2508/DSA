d607d95cc3c23a5f8defd8db2a60bc31f52ffd40
Ayush Giri <ayushgiri9988777@gmail.com>
2026-06-09 09:12:01 +0530

    Time: 32 ms (55.58%) | Memory: 72.5 MB (9.95%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,33 +1,17 @@
-// class Solution {
-// public:
-//     int findKthLargest(vector<int>& nums, int k) {
-//         priority_queue<int,vector<int>,greater<int>>pq;
-//         int n=nums.size();
-//         for(int i=0;i<k;i++){
-//             pq.push(nums[i]);
-//         }
-//         for(int i=k;i<n;i++){
-//             if(nums[i]>pq.top()){
-//                 pq.pop();
-//                 pq.push(nums[i]);
-//             }
-//         }
-//     return pq.top();
-//     }
-// };
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-
-        for (int num : nums) {
-            pq.push(num);
-
-            if (pq.size() > k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int n=nums.size();
+        for(int i=0;i<k;i++){
+            pq.push(nums[i]);
+        }
+        for(int i=k;i<n;i++){
+            if(nums[i]>pq.top()){
                 pq.pop();
+                pq.push(nums[i]);
             }
         }
-
-        return pq.top();
+    return pq.top();
     }
 };
