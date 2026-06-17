eec850a56cf648eab643e4f33e844e242a10116d
Ayush Giri <ayushgiri9988777@gmail.com>
2026-06-17 22:42:35 +0530

    Time: 3 ms (37.79%) | Memory: 14.3 MB (19.83%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-void solve (int start,vector<int>& candidates,int n,int target,vector<vector<int>>&res,vector<int>&ans){
-    if(target==0){
-     res.push_back(ans);
-        return;
-    }
-    for(int i=start;i<n;i++){
-        if(i > start && candidates[i] == candidates[i-1]) continue;
-        if(candidates[i]>target) break;
-       
-            ans.push_back(candidates[i]);
-        solve(i+1,candidates,n,target-candidates[i],res,ans);
-        ans.pop_back();      
+    vector<vector<int>> res;
+
+    void solve(int idx, vector<int>& candidates, int target, vector<int>& curr) {
+        if (target == 0) {
+            res.push_back(curr);
+            return;
+        }
+
+        for (int i = idx; i < candidates.size(); i++) {
+            // Skip duplicates
+            if (i > idx && candidates[i] == candidates[i - 1])
+                continue;
+
+            // No need to proceed further
+            if (candidates[i] > target)
+                break;
+
+            curr.push_back(candidates[i]);
+
+            // Move to next index since each element can be used only once
+            solve(i + 1, candidates, target - candidates[i], curr);
+
+            curr.pop_back(); // Backtrack
+        }
     }
 
-}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
         sort(candidates.begin(), candidates.end());
-        vector<int>ans;
-        int n= candidates.size();
-        int start=0;
-         solve( start,candidates,n,target,res,ans);
-      return res;
+
+        vector<int> curr;
+        solve(0, candidates, target, curr);
+
+        return res;
     }
 };
