4b56f38001be38d7ff8dfa69952b0406458ed383
Ayush Giri <ayushgiri9988777@gmail.com>
2026-07-25 19:15:58 +0530

    Time: 1092 ms (8.59%) | Memory: 7.8 MB (95.51%) - LeetSync

---
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int cnt=0;
+        if(maxDoubles==0){
+            return target-1;
+        }
+        while(target>0){
+        if(target%2!=0){
+            cnt++;
+             target-=1;
+        }
+        else if(target%2==0&&maxDoubles>0){
+            target/=2;
+            maxDoubles--;
+            cnt++;
+        }
+        else{
+            cnt++;
+            target-=1;
+        }
+        }
+        return cnt-1;
+    }
+};
