8767faea0088fc91cdd781b5d226926d0a7e6cae
Ayush Giri <ayushgiri9988777@gmail.com>
2026-07-25 19:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (2.05%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -1,25 +1,46 @@
+// class Solution {
+// public:
+//     int minMoves(int target, int maxDoubles) {
+//         int cnt=0;
+//         if(maxDoubles==0){
+//             return target-1;
+//         }
+//         while(target>0){
+//         if(target%2!=0){
+//             cnt++;
+//              target-=1;
+//         }
+//         else if(target%2==0&&maxDoubles>0){
+//             target/=2;
+//             maxDoubles--;
+//             cnt++;
+//         }
+//         else{
+//             cnt++;
+//             target-=1;
+//         }
+//         }
+//         return cnt-1;
+//     }
+// };
 class Solution {
 public:
     int minMoves(int target, int maxDoubles) {
-        int cnt=0;
-        if(maxDoubles==0){
-            return target-1;
+        int moves = 0;
+
+        while (target > 1 && maxDoubles > 0) {
+            if (target % 2 == 0) {
+                target /= 2;
+                maxDoubles--;
+            } else {
+                target--;
+            }
+            moves++;
         }
-        while(target>0){
-        if(target%2!=0){
-            cnt++;
-             target-=1;
-        }
-        else if(target%2==0&&maxDoubles>0){
-            target/=2;
-            maxDoubles--;
-            cnt++;
-        }
-        else{
-            cnt++;
-            target-=1;
-        }
-        }
-        return cnt-1;
+
+        // If no doubles remain, only decrement operations are possible
+        moves += (target - 1);
+
+        return moves;
     }
 };
