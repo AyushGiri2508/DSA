9a1b1428781d72f3b7cc1f110bd7694465f2afd0
Ayush Giri <ayushgiri9988777@gmail.com>
2026-08-16 09:27:34 +0530

    Time: 8 ms (6.77%) | Memory: 9.6 MB (76.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        int n=s.length();
-        int freq[26]={0};
-        for(int i=0;i<n;i++){
-            freq[s[i]-'a']++;
-            freq[t[i]-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0) return false;
-        }
-        return true;
+     sort(s.begin(),s.end());
+     sort(t.begin(),t.end());
+     if(s==t) return true;
+     
+     return false;
+
     }
 };
