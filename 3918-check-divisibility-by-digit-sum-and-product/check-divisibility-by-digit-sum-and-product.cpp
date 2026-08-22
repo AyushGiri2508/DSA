47440d7c9750a0120403f35f42edda14c2b38da0
Ayush Giri <ayushgiri9988777@gmail.com>
2026-08-22 18:29:48 +0530

    Time: 3 ms (4.09%) | Memory: 7.8 MB (37.42%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+int product(int n){
+    int prod=1;
+    while(n>0){
+        int bit=n%10;
+        prod*=bit;
+        n/=10;
+    }
+    return prod;
+}
+int digitsum(int n){
+    int sum=0;
+    while(n>0){
+        int bit=n%10;
+        sum+=bit;
+        n/=10;
+    }
+    return sum;
+}
+    bool checkDivisibility(int n) {
+        int val=digitsum(n)+product(n);
+        if(n%val==0)return true;
+
+      return false;  
+    }
+};
