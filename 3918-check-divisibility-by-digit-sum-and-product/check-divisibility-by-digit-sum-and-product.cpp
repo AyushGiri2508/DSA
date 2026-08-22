10dbfa2b96a0368dafaad5ed8b3dabcb0adc6898
Ayush Giri <ayushgiri9988777@gmail.com>
2026-08-22 18:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (8.80%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -1,27 +1,47 @@
+// class Solution {
+// public:
+// int product(int n){
+//     int prod=1;
+//     while(n>0){
+//         int bit=n%10;
+//         prod*=bit;
+//         n/=10;
+//     }
+//     return prod;
+// }
+// int digitsum(int n){
+//     int sum=0;
+//     while(n>0){
+//         int bit=n%10;
+//         sum+=bit;
+//         n/=10;
+//     }
+//     return sum;
+// }
+//     bool checkDivisibility(int n) {
+//         int val=digitsum(n)+product(n);
+//         if(n%val==0)return true;
+
+//       return false;  
+//     }
+// };
+
 class Solution {
 public:
-int product(int n){
-    int prod=1;
-    while(n>0){
-        int bit=n%10;
-        prod*=bit;
-        n/=10;
-    }
-    return prod;
-}
-int digitsum(int n){
-    int sum=0;
-    while(n>0){
-        int bit=n%10;
-        sum+=bit;
-        n/=10;
-    }
-    return sum;
-}
     bool checkDivisibility(int n) {
-        int val=digitsum(n)+product(n);
-        if(n%val==0)return true;
+        int sum = 0;
+        int product = 1;
+        int temp = n;
 
-      return false;  
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            sum += digit;
+            product *= digit;
+
+            temp /= 10;
+        }
+
+        return n % (sum + product) == 0;
     }
 };
