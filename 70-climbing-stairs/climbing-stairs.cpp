ac0c2c4c57031c2372b209c4db7a8c0644dea1ca
Ayush Giri <ayushgiri9988777@gmail.com>
2026-08-27 10:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (74.69%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -21,9 +21,25 @@ int solveusingmem(int n){
     }
     return dp[n];
 }
+int solveusingspace(int n){
+    if(n==1||n==0){
+        return 1;
+    }
+    int prev=1;
+
+    int curr=1;
+    int ans=0;
+    for(int i=2;i<=n;i++){
+        ans=prev+curr;
+        prev=curr;
+        curr=ans;
+    }
+    return ans;
+}
     int climbStairs(int n) {
         // vector<int>dp(n+1,-1);
     //     return solve(n,dp);
-   return solveusingmem(n);
+//    return solveusingmem(n);
+return solveusingspace(n);
     }
 };
