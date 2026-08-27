24ff7952c42898394c8a10d06027d914bc9a33d8
Ayush Giri <ayushgiri9988777@gmail.com>
2026-08-27 10:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,8 +12,18 @@ int solve(int n, vector<int>&dp){
     dp[n]= ans;
     return dp[n];
 }
+int solveusingmem(int n){
+    vector<int>dp(n+1,-1);
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int>dp(n+1,-1);
+    //     return solve(n,dp);
+   return solveusingmem(n);
     }
 };
