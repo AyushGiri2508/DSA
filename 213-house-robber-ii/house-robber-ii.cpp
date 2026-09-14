8e480614295f5dfbb07a9df41214a38f20f49812
Ayush Giri <ayushgiri9988777@gmail.com>
2026-09-15 00:54:50 +0530

    Time: 3 ms (7.06%) | Memory: 10.8 MB (53.47%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-int solve(vector<int>&arr,int i,int n,vector<int>&dp){
-   
-    if(i>=n){
+int solve(vector<int>&nums,int i,int end,vector<int>&dp){
+    if(i>end){
         return 0;
     }
     if(dp[i]!=-1){
         return dp[i];
     }
-    int include=arr[i]+solve(arr,i+2,n,dp);
-    int exclude=0+solve(arr,i+1,n,dp);
+    int include=nums[i]+solve(nums,i+2,end,dp);
+    int exclude=solve(nums,i+1,end,dp);
     dp[i]= max(include,exclude);
     return dp[i];
 }
     int rob(vector<int>& nums) {
+        int i=0;
         int n=nums.size();
-        if(n==1){
-            return nums[0];
-        }
-        vector<int>dp1(n+1,-1);
-         vector<int>dp2(n+1,-1);
-        int ans1=solve(nums,0,n-1,dp1);
-         int ans2 = solve(nums, 1, n,dp2);
-        return max(ans1,ans2);
+        if(n==1) return nums[0];
+           vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+        int case1 = solve(nums, 0, n - 2,dp1); // Exclude last
+        int case2 = solve(nums, 1, n - 1,dp2); // Exclude first
+
+        return max(case1, case2);
     }
 };
