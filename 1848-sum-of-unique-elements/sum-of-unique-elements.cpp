b3658a3ea20ba24a06d26a372f6fffd68793b8c6
Ayush Giri <ayushgiri9988777@gmail.com>
2026-09-17 09:53:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (17.27%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        map<int,int>mpp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        int sum=0;
+        for(auto val:mpp){
+            if(val.second==1){
+                sum+=val.first;
+            }
+        }
+        return sum;
+    }
+};
